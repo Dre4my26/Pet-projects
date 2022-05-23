b8da2855b4c408a0e9ef54d653770a7d13a17e00
Dre4my26 <dreamyguy26@gmail.com>
2022-05-23 21:07:41 +0300

    updated Bot.py, now it can understand and reply to any user's message

--- PricesParser/Bot.py
+++ PricesParser/Bot.py
@@ -1,11 +1,18 @@
 import telebot
 
-bot = telebot.TeleBot("5320442299:AAG0L8KCm5zTVeU66taTDM-TTi3BkeARFYY")  # You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot(
+    "5320442299:AAG0L8KCm5zTVeU66taTDM-TTi3BkeARFYY")  # You can set parse_mode by default. HTML or MARKDOWN
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
     if message.text == "/start":
-        bot.send_message(message.from_user.id, "Привет! Чем я могу тебе помочь?")
+        bot.send_message(message.chat.id, "Привет! Чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.chat.id, "Я пока не умею помогать((")
+
+@bot.message_handler(func= lambda message:True)
+def echo_all(message):
+    bot.reply_to(message, "Прости, я тебя не понимаю, попробуй написать /help ")
 
 bot.polling(none_stop=True, interval=0)
