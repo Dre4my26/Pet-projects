3d4c30d40c4b3af0ac7e80109a797730a6709fcc
Dre4my26 <dreamyguy26@gmail.com>
2022-06-07 01:53:07 +0300

    split func parser into parser_price and parser_desc to add to cart in future

--- PricesParser/bs4_test.py
+++ PricesParser/bs4_test.py
@@ -6,12 +6,13 @@ That is more or less working way to parse prices from sites different from Ozon,
 """
 
 
-def parser(link):
-    price = 0
-    #print("please, insert the link:")
-    #link = str(input())
+def parser_price(link):
+    """This function returns the price of an item"""
+    price = 'Error when attempted to get price'
+    # print("please, insert the link:")
+    # link = str(input())
     if "www.ozon.ru" in link:
-        price = "Прости, я пока не умею узнавать цены с озона("
+        price = "Прости, я не умею узнавать цены с озона("
     else:
         r = requests.get(link).text
         soup = BeautifulSoup(r, "html.parser")
@@ -19,7 +20,28 @@ def parser(link):
         text_from_site = str(soup)
         text_from_site = text_from_site.split(" ")
         for i in range(len(text_from_site)):
-            if text_from_site[i] == "Цена" or text_from_site[i] == "цена":
-                price = int(text_from_site[i + 1])
+            if text_from_site[i] == "Цена" or text_from_site[i] == "цена" or text_from_site[i] == "цене" or \
+                    text_from_site[i] == "Цене":
+                try:
+                    price = int(text_from_site[i + 1])
+                except ValueError:
+                    price = 'Failed to find the price'
+                    print('InvalidParseResultError occurred!')
 
     return price
+
+
+def parser_desc(link):
+    """This func returns the Title of an item"""
+    r = requests.get(link).text
+    soup = BeautifulSoup(r, "html.parser")
+    title = soup.title.text
+
+    return title
+
+
+def cartSaver(chat_id):
+    chat = []
+    chat.append(chat_id)
+    for chat[chat_id] in chat:
+        cart = {'name': parser_desc(), 'price': parser_price()}
