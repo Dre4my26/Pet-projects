fbbb93fa6adb5012f402585048633360abe08e44
Dre4my26 <dreamyguy26@gmail.com>
2022-06-07 02:01:52 +0300

    added some changes

--- PricesParser/Bot.py
+++ PricesParser/Bot.py
@@ -4,7 +4,7 @@ Code for @YetAnotherPriceCheckerBot
 
 import telebot
 from telebot import types
-from bs4_test import parser
+from bs4_test import parser_price, cartSaver
 
 bot = telebot.TeleBot(
     "5320442299:AAG0L8KCm5zTVeU66taTDM-TTi3BkeARFYY")  # You can set parse_mode by default. HTML or MARKDOWN
@@ -15,11 +15,17 @@ def start(message):
     if message.text == "/start":
         bot.send_message(message.chat.id, "Привет! Я - бот для парсинга цен и их суммирования. Моя прелесть состоит в "
                                           "том, что я могу <i>суммировать цены на товары с разных сайтов</i>. Для"
-                                          " парсинга цены с какого-либо сайта введи команду <b>/price</b>",
+                                          " парсинга цены с какого-либо сайта введи команду <b>/price</b>."
+                                          "<i>\nУвы, я не умею парсить Ozon, все остальное в Вашем распоряжении!</i>",
                          parse_mode="html")
 
 
-@bot.message_handler(commands=['price'])  # for photos user sends
+@bot.message_handler(commands=['cart'])
+def cart(message):
+    cartSaver(message.chat.id)
+
+
+@bot.message_handler(commands=['price'])
 def get_user_price(message):
     msg = bot.send_message(message.chat.id, 'Введите ссылку на товар:')
     bot.register_next_step_handler(msg, get_user_text)
@@ -27,25 +33,20 @@ def get_user_price(message):
 
 def get_user_text(message):
     if 'http' in message.text:
-        bot.send_message(message.chat.id, parser(message.text))
+        bot.send_message(message.chat.id, parser_price(message.text))
+
     else:
         bot.reply_to(message, "Ссылка невалидна!")
-
-
-@bot.message_handler(commands=['website'])
-def website(message):
-    markup = types.InlineKeyboardMarkup()  # created in-text buttons
-    markup.add(types.InlineKeyboardButton("Страница создателя", url="https://vk.com/dreamy26"))
-    bot.send_message(message.chat.id, 'Глянь на это!', reply_markup=markup)
 
 
 @bot.message_handler(commands=['help'])
 def buttons(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)  # created user buttons
-    website_user_command = types.KeyboardButton('/website')
     start_user_command = types.KeyboardButton('/start')
+    cart_user_command = types.KeyboardButton('/cart')
 
-    markup.add(website_user_command, start_user_command)
+    markup.add(start_user_command)
+    markup.add(cart_user_command)
     bot.send_message(message.chat.id, 'Глянь на это!', reply_markup=markup)
 
 
